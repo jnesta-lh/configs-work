9416dc514cbb491ca0f81d3288eec96b45662f98
James Nesta <jnesta@logixhealth.com>
2024-08-09 10:22:27 -0400

    updates

--- copy-configs.sh
+++ copy-configs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail # Exit on errors and undefined variables.
+
 # Get the directory of this script:
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -13,3 +15,6 @@ cp "/c/Users/jnesta/OneDrive - LogixHealth Inc/Documents/Scripts/AutoHotkey.ahk"
 cp "/c/Users/jnesta/AppData/Roaming/Code/User/settings.json" "$DST_DIR/"
 
 git add --all && git commit -m "updates" && git pull && git push
+
+LATEST_SHA1=$(git rev-parse HEAD)
+start chrome "https://github.com/jnesta-lh/configs/commit/$LATEST_SHA1"
