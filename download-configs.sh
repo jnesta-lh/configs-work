230c9202914e11adeeb74fdd5472a55ec1db7afc
James Nesta <jnesta@logixhealth.com>
2025-02-20 21:31:35 -0500

    chore: update download script

--- download-configs.sh
+++ download-configs.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail # Exit on errors and undefined variables.
 
-curl "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/.bashrc" --output ~/.bash_extras
+curl "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/.bashrc" --output ~/.bash_extras --silent
