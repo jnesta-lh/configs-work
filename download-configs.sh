ed14574001e984d890d402ab45be3a21206d1c32
James Nesta <jnesta@logixhealth.com>
2025-04-12 15:38:34 -0400

    chore: update download-configs script

--- download-configs.sh
+++ download-configs.sh
@@ -10,6 +10,8 @@ cd "$DIR"
 
 git pull
 
+echo "GETTING HERE 1"
+
 curl --silent "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/AutoHotkey.ahk" --output "/c/Users/jnesta/OneDrive - LogixHealth Inc/Documents/Scripts/AutoHotkey.ahk"
 curl --silent "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/keybindings.json" --output "/c/Users/jnesta/AppData/Roaming/Code/User/keybindings.json"
 curl --silent "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/settings.json" --output "/c/Users/jnesta/AppData/Roaming/Code/User/settings.json"
