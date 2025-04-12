adfa3eedc6171575cd79769cb91e933c0179503d
James Nesta <jnesta@logixhealth.com>
2025-04-12 15:41:16 -0400

    chore: cleanup

--- download-configs.sh
+++ download-configs.sh
@@ -10,8 +10,6 @@ cd "$DIR"
 
 git pull
 
-echo "GETTING HERE 1"
-
 curl --silent "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/AutoHotkey.ahk" --output "/c/Users/jnesta/OneDrive - LogixHealth Inc/Documents/Scripts/AutoHotkey.ahk"
 curl --silent "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/keybindings.json" --output "/c/Users/jnesta/AppData/Roaming/Code/User/keybindings.json"
 curl --silent "https://raw.githubusercontent.com/jnesta-lh/configs/refs/heads/main/configs/settings.json" --output "/c/Users/jnesta/AppData/Roaming/Code/User/settings.json"
